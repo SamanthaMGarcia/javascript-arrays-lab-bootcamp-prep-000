de1d25f5a2fad58719cde820a6881937c084d8e7
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-05 23:50:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 var kittens = ['Milo', 'Otis', 'Garfield']
 function destructivelyAppendKitten(array, name) {
-  array.append(name)
+  array.push(name)
   return array
 }
